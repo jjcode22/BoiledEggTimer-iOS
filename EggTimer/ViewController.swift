f744b04f6f09e7fe528f7e86c415e6602b18398e
jjcode22 <jjmcoding31@gmail.com>
2024-01-30 00:35:35 +0530

    Selected egg status

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,16 +2,17 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var subTitle: UILabel!
+    @IBOutlet weak var softImage: UIImageView!
     @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var mediumImage: UIImageView!
+    @IBOutlet weak var hardImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     var player: AVAudioPlayer!
     
@@ -26,6 +27,51 @@ class ViewController: UIViewController {
         timer.invalidate()
         let hardness = sender.currentTitle!
         totalTime = eggTimes[hardness]!
+        
+        switch hardness {
+        case "Soft":
+            hardImage.layer.shadowColor = UIColor.gray.cgColor
+            hardImage.layer.shadowOpacity = 0
+           
+            mediumImage.layer.shadowColor = UIColor.gray.cgColor
+            mediumImage.layer.shadowOpacity = 0
+           
+            softImage.layer.shadowColor = UIColor.black.cgColor
+            softImage.layer.shadowOpacity = 0.5
+            
+            
+        case "Medium":
+            softImage.layer.shadowColor = UIColor.gray.cgColor
+            softImage.layer.shadowOpacity = 0
+            
+            hardImage.layer.shadowColor = UIColor.gray.cgColor
+            hardImage.layer.shadowOpacity = 0
+            
+            mediumImage.layer.shadowColor = UIColor.black.cgColor
+            mediumImage.layer.shadowOpacity = 0.5
+            
+            
+        case "Hard":
+            softImage.layer.shadowColor = UIColor.gray.cgColor
+            softImage.layer.shadowOpacity = 0
+            
+            mediumImage.layer.shadowColor = UIColor.gray.cgColor
+            mediumImage.layer.shadowOpacity = 0
+            
+            hardImage.layer.shadowColor = UIColor.black.cgColor
+            hardImage.layer.shadowOpacity = 0.5
+            
+        default:
+            hardImage.layer.shadowColor = UIColor.gray.cgColor
+            hardImage.layer.shadowOpacity = 0
+            
+            softImage.layer.shadowColor = UIColor.gray.cgColor
+            softImage.layer.shadowOpacity = 0
+            
+            mediumImage.layer.shadowColor = UIColor.gray.cgColor
+            mediumImage.layer.shadowOpacity = 0
+           
+        }
         
         progressBar.progress = 0.0
         secondsPassed = 0
